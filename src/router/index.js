580b035eef17180dc8cb5bb9378df3d9408aec09
Nghia Hoang <nghiahoang880@gmail.com>
2025-06-25 12:43:48 +0700

    feat: create 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,12 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
+
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
     },
 ];
 const router = createRouter({
